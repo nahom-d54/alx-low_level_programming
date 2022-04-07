f9016888f807398ed794228b062c712ad72542db
Nahom <nahom.d54@gmail.com>
2022-04-07 06:46:57 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main()
+
+/* 
+ * main - Entry Point
+ *
+ * puts is same as printf i think
+ *
+ * char var[num] : char defines character and num is number of array -1 in the variale
+ *
+ */
+int main(void)
 {
 	char toPrint[100] = "\"Programming is like building a multilingual puzzle";
 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	char toPrint[100] = "\"Programming is like building a multilingual puzzle";
+
+	puts(toPrint);
+
+	return (0);
+}
