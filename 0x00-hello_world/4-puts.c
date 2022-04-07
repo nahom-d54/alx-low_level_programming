854488037055cce97e768a438a90b379e8dcd714
Nahom <nahom.d54@gmail.com>
2022-04-07 07:15:21 -0700

    betty please

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-/** 
+/**
  * main - Entry Point
- *
  * puts: prints stuff
- *
  * char var[num] : char defines character
- *
  */
 int main(void)
 {
 	char toPrint[100] = "\"Programming is like building a multilingual puzzle";
+
 	puts(toPrint);
 	return (0);
 }
