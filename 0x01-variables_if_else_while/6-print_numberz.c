126bc73686c97b3bd6489fd4307c1521293e8800
Nahom <nahom.d54@gmail.com>
2022-04-08 08:13:50 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints alphabets
+ * small letters
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar('0'+a);
+	}
+	putchar('\n');
+	return (0);
+}
