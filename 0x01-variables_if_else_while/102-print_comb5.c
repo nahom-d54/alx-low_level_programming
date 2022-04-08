9abdcdb44df4877d375a8f33faa6de7dd035f454
Nahom <nahom.d54@gmail.com>
2022-04-08 12:42:13 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints alphabets
+ * small letters
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{	
+			putchar('0' + a / 10);
+			putchar('0' + a % 10);
+			putchar(' ');
+			putchar('0' + b / 10);
+			putchar('0' + b % 10);
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
