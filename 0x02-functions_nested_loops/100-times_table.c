51a6d586ceeb03318abec0d755ebd5d86edbe463
nahom-d54 <101549562+nahom-d54@users.noreply.github.com>
2022-04-13 16:48:37 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,48 @@
 #include "main.h"
 
 /**
- * print_times_table - prints times from 0 to 15 max
+ * print_times_table - prints the n times tabel
  * @n: number of the times table
+ * num1 ,num2 , num3 arguments
  */
 void print_times_table(int n)
 {
-    int num1, num2, num3;
+	int num1, num2, num3;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (num1 = 0; num1 <= n; num1++)
-        {
-            for (num2 = 0; num2 <= n; num2++)
-            {
-                num3 = num2 * num1;
-                if (num2 == 0)
-                {
-                    _putchar(num3 + '0');
-                } else if (num3 < 10 && num2 != 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(num3 + '0');
-                } else if (num3 >= 10 && num3 < 100)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar((num3 / 10) + '0');
-                    _putchar((num3 % 10) + '0');
-                } else if (num3 >= 100)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar((num3 / 100) + '0');
-                    _putchar(((num3 / 10) % 10) + '0');
-                    _putchar((num3 % 10) + '0');
-                }
-            }
-            _putchar('\n');
-        }
-    }
+	if (n >= 0 && n <= 15)
+	{
+		for (num1 = 0; num1 <= n; num1++)
+		{
+			for (num2 = 0; num2 <= n; num2++)
+			{
+				num3 = num2 * num1;
+				if (num2 == 0)
+				{
+					_putchar(num3 + '0');
+				} else if (num3 < 10 && num2 != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(num3 + '0');
+				} else if (num3 >= 10 && num3 < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((num3 / 10) + '0');
+					_putchar((num3 % 10) + '0');
+				} else if (num3 >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((num3 / 100) + '0');
+					_putchar(((num3 / 10) % 10) + '0');
+					_putchar((num3 % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
