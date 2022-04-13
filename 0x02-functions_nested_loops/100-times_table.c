91e679505f91dba1143e640ae028a5d2db748a0b
Nahom <nahom.d54@gmail.com>
2022-04-13 06:26:13 -0700

    advanced 1 and 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,47 @@
 #include "main.h"
-/**
- * times_table - prints times table
- * Return:void
- */
 
+/**
+ * print_times_table - prints times from 0 to 15 max
+ * @n: number of the times table
+ */
 void print_times_table(int n)
 {
+    int num1, num2, num3;
 
-	int a = 0;
-	int b = 0;
-	int rep;
-	
-	if (n < 0 || n > 16)
-		return
-	while (a <= n)
-	{
-
-		while (b <= n)
-		{
-			rep = a * b;
-			if (b == 0)
-			{
-				_putchar('0' + rep);
-			}
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar('0' + rep);
-			}
-			else
-			{
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-
-			if (b < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			b++;
-		}
-		_putchar('\n');
-		a++;
-	}
-
+    if (n >= 0 && n <= 15)
+    {
+        for (num1 = 0; num1 <= n; num1++)
+        {
+            for (num2 = 0; num2 <= n; num2++)
+            {
+                num3 = num2 * num1;
+                if (num2 == 0)
+                {
+                    _putchar(num3 + '0');
+                } else if (num3 < 10 && num2 != 0)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(num3 + '0');
+                } else if (num3 >= 10 && num3 < 100)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar((num3 / 10) + '0');
+                    _putchar((num3 % 10) + '0');
+                } else if (num3 >= 100)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar((num3 / 100) + '0');
+                    _putchar(((num3 / 10) % 10) + '0');
+                    _putchar((num3 % 10) + '0');
+                }
+            }
+            _putchar('\n');
+        }
+    }
 }
