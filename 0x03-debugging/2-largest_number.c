3fed6f2826942338068f402076991495780270d8
Nahom <nahom.d54@gmail.com>
2022-04-14 13:32:52 -0700

    argest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,26 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	} else if (a > c && c > b)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	} else if (b > c && c > a)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	else if (c > a && a > b)
+	else
 	{
 		largest = c;
-	} else if (c > b && b > a)
-	{
-		largest = c
 	}
 
 	return (largest);
