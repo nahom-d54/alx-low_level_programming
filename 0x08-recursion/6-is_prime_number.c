6f6b178c0c268ae3f0832ce9566de54681fb182b
Nahom <nahom.d54@gmail.com>
2022-04-27 05:11:15 -0700

    some fixes

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,29 @@
-#include "main.h"
+/**
+ * is_it_really - a function to recrusivly 
+ * check
+ *
+ * @n: number to check
+ * @c: loop kind of thing number
+ */
+int is_it_really(int n, int c)
+{
+	if (c < 10)
+		if (n % c == 0)
+			return (0);
+		else
+			is_it_really(n, c + 1);
+	else
+		return (1);
+
+}
 /**
  * is_prime_number - a function to check if prime
  *
  * @n: number to check
- * Return: return 1 if prime 0 if not
  */
 int is_prime_number(int n)
 {
-	int c;
-
-	if (n <= 1)
+    if (n <= 1)
 		return (0);
-	for (c = 2; c < 10; c++)
-	{
-		if (n % c == 0)
-			return (0);
-	}
-	return (1);
+	return (is_it_really(n, 2));
 }
