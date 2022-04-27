986e05879d46d7b827a24b155b1dc960e872ca43
Nahom <nahom.d54@gmail.com>
2022-04-27 05:19:56 -0700

    recrusive fix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,20 @@
+#include "main.h"
 /**
- * is_it_really - a function to recrusivly 
+ * is_it_really - a function to recrusivly
  * check
  *
  * @n: number to check
  * @c: loop kind of thing number
+ * Return: returns result to prime function
  */
 int is_it_really(int n, int c)
 {
 	if (c < 10)
+	{
 		if (n % c == 0)
 			return (0);
-		else
-			is_it_really(n, c + 1);
+		is_it_really(n, c + 1);
+	}
 	else
 		return (1);
 
@@ -20,10 +23,11 @@ int is_it_really(int n, int c)
  * is_prime_number - a function to check if prime
  *
  * @n: number to check
+ * Return: returns value to main function
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
+	if (n <= 1)
 		return (0);
 	return (is_it_really(n, 2));
 }
