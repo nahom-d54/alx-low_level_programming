18a02eb9da6ca021034c9e99d891017ac2ae4f29
Nahom <nahom.d54@gmail.com>
2022-05-03 03:44:11 -0700

    string dump

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - a function that returns a pointer
+ *
+ * @str: the string to aloocate the memory to
+ * Return: returns null or pointer
+ */
+char *_strdup(char *str)
+{
+	char *to;
+	int size = 0;
+	int alloc;
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	to = malloc(sizeof(char) * size + 1);
+	if (!to)
+		return (NULL);
+	for (alloc = 0; alloc < size; alloc++)
+	{
+		to[alloc] = str[alloc];
+	}
+	return (to);
+}
