29ea01c7383db03441c91db6597b9193bfd8c204
Nahom <nahom.d54@gmail.com>
2022-05-03 04:47:49 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees 2 dimentional grid
+ * @grid: the grid to free
+ * @height: height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	unsigned int h = 0
+
+	while (h < height)
+	{
+		free(grid[h]);
+		h++;
+	}
+	free(grid);
+}
