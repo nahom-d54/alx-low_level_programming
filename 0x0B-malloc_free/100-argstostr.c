bf745ac2756c2b7853120d6ca90f73988af3bf42
Nahom <nahom.d54@gmail.com>
2022-05-03 21:43:44 -0700

    advanced

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates ac strings.
+ * @av: argument value string
+ * @ac: argument count
+ * Return: pointer of an array of chars
+ */
+char *argstostr(int ac, char **av)
+{
+	char *strout;
+	unsigned int i = 0, j = 0, k = 0, l;
+
+	for (l = 0; l < ac; l++)
+	{
+		if (av[l] == NULL)
+			av[l] == "";
+		for (i = 0; av[l][i] != '\0'; i++)
+		{
+			j++;
+		}
+	}
+
+	strout = malloc(sizeof(char) * j + ac);
+
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+
+	for (l = 0; l < ac; l++)
+	{
+		for (i = 0; av[l][i] != '\0'; i++)
+		{
+			if (k < j)
+			{
+				strout[k] = av[l][i];
+				k++;
+			}
+		}
+		strout[k] = '\n';
+		k++;
+	}
+	return (strout);
+}
